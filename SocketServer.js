aace15841e28ed74e57d4fa2e132eccd9d21decf
wowgarcon <herbing@naver.com>
2020-09-26 18:06:25 +0900

    modify name of module variables

--- SocketServer.js
+++ SocketServer.js
@@ -1,14 +1,15 @@
-const app = require('express')();
+const express = require('express');
+const app = express();
 const http = require('http').createServer(app);;
+const fs = require('fs');
 const launcher = require('./js/launcher');
 const cluster = require('./js/config/cluster');
-const fs = require('fs');
 const port = 1004;
 
 let worker = cluster(app);
 if(worker){
-    // launcher(app, port);
-    app.listen(port, () => {
+    const server = http.listen(port, () => {
         console.log(`Server is listening at localhost '${port}'`);
     });
+    launcher(server);
 }
